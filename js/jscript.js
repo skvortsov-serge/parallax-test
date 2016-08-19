57dbd58d48e03c6550aa4897a39cdf8c3fed4aa1
Sergey Skvortsov <skvortsov-serge@gmail.com>
2016-08-19 13:27:42 +0300

    first commit

---
+++ js/jscript.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        var wScroll = $(this).scrollTop();
+        $('.logo').css({
+            'transform': 'translate(0, ' + wScroll / 2 + '%)'
+        });
+        $('.fore-bird').css({
+            'transform': 'translate(0, -' + wScroll / 40 + '%)'
+        });
+
+        $('.back-bird').css({
+            'transform': 'translate(0, ' + wScroll / 4 + '%)'
+        });
+
+        if (wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
+        	$('.clothes-pics figure').each(function(i) {
+
+        		setTimeout(function() {
+        		$('.clothes-pics figure').eq(i).addClass('is-showing');
+        			
+        		}, 250 * (i+1));
+        	});
+        }
+
+        if (wScroll > $('.large-window').offset().top - $(window).height()) {
+        	$('.large-window').css({'background-position': 'center '+(wScroll - $('.large-window').offset().top + -190)+'px'});
+        	var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 6) ;
+
+        	$('.window-tint').css({'opacity': opacity});
+        }
+
+         if (wScroll > $('.blog-posts').offset().top - $(window).height()) {
+
+         	var offset = Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height() - 350);
+
+         	$('.post-1').css({'transform': 'translate('+offset+'px, '+Math.abs(offset * 0.2)+'px)'});
+
+         	$('.post-3').css({'transform': 'translate('+Math.abs(offset)+'px, '+Math.abs(offset * 0.2)+'px)'});
+
+         }
+
+    });
+
+
+});
